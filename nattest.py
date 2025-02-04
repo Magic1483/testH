381603a317e98db51314ba6dae0d4fda10672573
net-user <net-user@yahoo.com>
2025-02-04 08:36:20 -0800

    test -20

--- nattest.py
+++ nattest.py
@@ -8,24 +8,33 @@ import random
 STUN_SERVER = "stun.l.google.com"
 STUN_PORT = 19302
 
-def get_external_ip_port():
+stun_list = [
+    "stun.l.google.com:19302",
+    "stun1.l.google.com:1930",
+    "stun2.l.google.com:1930",
+    "stun3.l.google.com:1930",
+    "stun4.l.google.com:1930"
+]
+
+def get_external_ip_port(host,port):
     """Query STUN server to get external IP and port."""
     nat_type, external_ip, external_port = py3stun.get_ip_info(
-        stun_host=STUN_SERVER, stun_port=STUN_PORT
+        stun_host=host, stun_port=port
         )
     return nat_type, external_ip, external_port
 
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-t','--timeout')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-t','--timeout')
+# args = parser.parse_args()
 
-print(f'Timeout {args.timeout}s')
-for i in range(25):
-    _,ip,port = get_external_ip_port()
+# print(f'Timeout {args.timeout}s')
+for i in stun_list:
+    stun_host,stun_port = i.split(':')
+    _,ip,port = get_external_ip_port(stun_host,int(stun_port))
     print(f'[{i+1}]',ip,port)
-    time.sleep(int(args.timeout))
+    time.sleep(0.1)
 
 
     
