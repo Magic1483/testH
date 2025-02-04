a221cd685cabee2c8ff61ea1775c1b4865d5390b
net-user <net-user@yahoo.com>
2025-02-04 08:51:52 -0800

    test -21

---
+++ test.py
@@ -0,0 +1,17 @@
+# client.py
+import py3stun
+import socket
+import time
+import random
+
+RENDEZVOUS_SERVER = ("83.147.245.51", 50000)
+
+def register_with_rendezvous(client_id, external_ip, external_port):
+    """Send NAT mapping info to the rendezvous server."""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect(RENDEZVOUS_SERVER)
+        sock.send(f"REGISTER:{client_id}:{external_ip}:{external_port}".encode())
+
+for i in range(25):
+    register_with_rendezvous('a','ip',233)
+    time.sleep(5)
